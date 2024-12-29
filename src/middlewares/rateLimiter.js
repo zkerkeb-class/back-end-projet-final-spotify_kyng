9b885b92f5522b5d5ff0b0a33b9064e1a4896285
Yasmineibr1 <yasmine.brahimi321@gmail.com>
2024-12-29 21:36:27 +0100

    rate limiting route test

--- src/middlewares/rateLimiter.js
+++ src/middlewares/rateLimiter.js
@@ -1,21 +1,23 @@
 const rateLimit = require('express-rate-limit');
-const RedisStore = require('rate-limit-redis');
+const { RedisStore } = require('rate-limit-redis');
 const Redis = require('ioredis');
 
+// Configure le client Redis
 const redisClient = new Redis({
-  host: process.env.REDIS_HOST ,
-  port: process.env.REDIS_PORT 
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
 });
 
+// Crée le middleware de limitation de débit
 const globalRateLimiter = rateLimit({
   store: new RedisStore({
-    sendCommand: (...args) => redisClient.call(...args), 
+    sendCommand: (...args) => redisClient.call(...args), // Correction importante ici
   }),
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, 
+  max: 10, // Nombre max de requêtes
   message: 'Too many requests from this IP, please try again later.',
   standardHeaders: true, // Retourne les en-têtes RateLimit-* standard
-  legacyHeaders: false,  // Désactive les en-têtes X-RateLimit-*
+  legacyHeaders: false, // Désactive les en-têtes X-RateLimit-*
 });
 
 module.exports = globalRateLimiter;
--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,8 @@ const {
 } = require('../controllers/songGenerator.controller');
 const { seedDatabase } = require('../controllers/seed.controller');
 const seedRoutes = require('../routes/seed.route');
+const globalRateLimiter = require('../middlewares/rateLimiter');
+const routeTest = require('./testroute');
 
 // // Route for generating a specific number of MP3 metadata entries
 // router.get('/song', generateMp3MetadataController);
@@ -14,5 +16,8 @@ const seedRoutes = require('../routes/seed.route');
 // router.get('/song/bulk', bulkGenerateMp3MetadataController);
 
 router.use('/seed', seedRoutes);
+
+router.use(globalRateLimiter); // Appliquer le middleware de rate limiting globalement
+router.use(routeTest);
 
 module.exports = router;
