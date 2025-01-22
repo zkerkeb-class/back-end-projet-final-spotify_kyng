f644d4679de4e6bf9db7441885e5d7e8455b45dc
Yasmineibr1 <yasmine.brahimi321@gmail.com>
2025-01-22 14:18:49 +0100

    redis config update

--- src/config/redis.js
+++ src/config/redis.js
@@ -2,7 +2,7 @@ const Redis = require('ioredis');
 
  const redisUrl = process.env.REDIS_URL;
 const redisClient = new Redis(redisUrl);
- enableOfflineQueue: false,
+ //enableOfflineQueue: false,
 
 redisClient.on('connect', () => console.log('Redis connecté'));
 redisClient.on('error', (err) => console.error('Erreur Redis', err));
